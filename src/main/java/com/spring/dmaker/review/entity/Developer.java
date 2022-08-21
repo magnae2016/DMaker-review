2839916b34c6d2b018bd05dbccc00be95f299d88
magnae2016 <magnae2016@gmail.com>
2022-08-21 17:50:31 +0900

    Add Developer Entity and id property related to @Id / @GeneratedValue

---
+++ src/main/java/com/spring/dmaker/review/entity/Developer.java
@@ -0,0 +1,20 @@
+package com.spring.dmaker.review.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Developer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+}
