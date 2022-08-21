3d4163b1f715f0b8c77f4156f72b1ab4cc7e98d5
magnae2016 <magnae2016@gmail.com>
2022-08-21 18:10:03 +0900

    Add Repository for Developer

---
+++ src/main/java/com/spring/dmaker/review/repository/DeveloperRepository.java
@@ -0,0 +1,9 @@
+package com.spring.dmaker.review.repository;
+
+import com.spring.dmaker.review.entity.Developer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+}
